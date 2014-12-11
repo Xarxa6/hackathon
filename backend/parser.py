5e5bccba053e27c781f81cbdf526e1fed3644d65
Nan <nan@tokbox.com>
2014-12-11 11:24:29 -0800

    parsing out dimension and metric

--- backend/parser.py
+++ backend/parser.py
@@ -1,10 +1,54 @@
 import nltk
-nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk import stem
+from nltk.tag import pos_tag
+
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('maxent_treebank_pos_tagger')
+# nltk.download('all-corpora')
+
 
 def parse_request(sentence):
-	try:
-		tokens = nltk.word_tokenize(sentence)
-		print "NLTK Parsing sentence..."
-	except:
-		print "There was an error when NLTK parsing!"
-	return tokens
+	tags = {}
+	tokens = nltk.word_tokenize(sentence)
+	snowball = stem.snowball.EnglishStemmer()
+	tokens_list = [snowball.stem(x.lower()) for x in tokens if x not in stopwords.words('english') and len(x) > 1]
+	# organization_list = getPartner(sentence)
+
+	dimension_list = getDimension(sentence)
+	tags['dimension'] = dimension_list
+	for item in dimension_list:
+		tokens_list.remove(item)
+	tags['metric']=tokens_list
+	print tags
+	return tags
+
+
+def getPartner(text):
+	organization_list = []
+	for sent in nltk.sent_tokenize(text):
+		for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+			if hasattr(chunk, 'node'):
+				if chunk.node == "ORGANIZATION":
+					organization = ' '.join(c[0] for c in chunk.leaves())
+					organization_list.append(organization)
+	print organization_list
+
+
+def getDimension(text):
+	date_list = []
+	for sent in nltk.sent_tokenize(text):
+		for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+			print chunk
+			if hasattr(chunk, 'node'):
+				if chunk.node == "DATE":
+					date = ' '.join(c[0] for c in chunk.leaves())
+					date_list.append(date)
+	return date_list
+
+text = "what is the Minerva minutes of yesterday"
+parse_request(text)
+
+
