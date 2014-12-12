86a06f28bf092e40ea408b7b488ebdbbe503da48
Eric <etmcmillen@gmail.com>
2014-12-12 13:31:17 -0800

    Added index to api & updated index.html

--- backend/static/LandingPage.js
+++ backend/static/LandingPage.js
@@ -2,34 +2,20 @@
  * Created by eric on 12/10/14.
  */
 
-
-
-
 var landingPage = (function () {
-    function highlightCode (code){
-        var returnString = "";
 
-        if (code) {
-            returnString = "";
-            returnString += '<pre class="language-sql">';
-            returnString += code;
-            returnString += "</pre>";
-        }
-
-        return returnString;
-    }
 
     function drawTable (collection){
 
-        var textVal = '<table class="table"><tr><th>Analysis ID</th><th>Tags</th></th><th>Type</th><th>Content</th></tr>';
+        var textVal = '<table class="table"><tr><th>Analysis ID</th><th>Tags</th></th><th>Payload</th><th>Status</th></tr>';
         var i;
 
         for (i=0; i<collection["content"].length; i++) {
             textVal += "<tr>";
             textVal += "<td>" + collection["content"][i]["analysis_id"] + "</td>";
-            textVal += "<td>" + JSON.stringify(collection["content"][i]["tags"].join(" ")) + "</td>";
-            textVal += "<td>" + collection["content"][i]["type"] + "</td>";
-            textVal += "<td>" + highlightCode(collection["content"][i]["sql"]) + "</td>";
+            textVal += "<td>" + JSON.stringify(collection["content"][i]["tags"]) + "</td>";
+            textVal += "<td>" + JSON.stringify(collection["content"][i]["payload"]) + "</td>";
+            textVal += "<td>" + collection["content"][i]["status"]+ "</td>";
             textVal += "</tr>";
         }
 
@@ -48,32 +34,30 @@ var landingPage = (function () {
 
         var ajaxSettings = {
             async       :  true,
-            dataType    : "application/json",
             contentType : "json",
             data        :  {"request": queryString },
             type        :  "GET"};
 
         $.ajax("/api/v1.0/analysis", ajaxSettings)
             .done(function (collection) {
-                var settings = collection;
-                console.log(collection);
                 drawTable(collection);
-            });
-    };
+             });
+     };
+
 
     var submitDispleasure = function (){
         var queryString = $("#searchQuery").val();
+        console.log(queryString);
 
         var ajaxSettings = {
-            async     :  true,
-            dataType  : "json",
-            data      :  {"request": queryString },
-            type      :  "POST"};
+            async       :  true,
+            contentType : "application/json",
+            data        :  {"request": queryString },
+            type        :  "POST"};
 
         $.ajax("/api/v1.0/analysis", ajaxSettings)
             .done(function (collection) {
                 var settings = collection;
-                console.log(collection);
                 drawTable(collection);
             });
     };
