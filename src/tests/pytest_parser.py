9390c0ee9792344b806edea3777547bb9b48ab08
Ernest <lordnesi28@gmail.com>
2014-12-26 00:58:15 +0100

    testing wsgi

--- src/tests/pytest_parser.py
+++ src/tests/pytest_parser.py
@@ -5,22 +5,23 @@ def all_in(a,b):
     assert(all([ item in a for item in b ]))
     assert(all([ item in b for item in a ]))
 
-class PytestParser:
+# class PytestParser:
 
-    def pytest_1(self):
-        parsed = parser.parse_request("registrations monthly")
-        expected = [{"measure":"registrations"},{"dimension":"monthly"}]
-        all_in(parsed,expected)
+    # TEMPORARY COMMENT TO TEST WSGI
+    # def pytest_1(self):
+    #     parsed = parser.parse_request("registrations monthly")
+    #     expected = [{"measure":"registrations"},{"dimension":"monthly"}]
+    #     all_in(parsed,expected)
 
-    def pytest_2(self):
-        parsed = parser.parse_request("users last month")
-        expected = [{"measure":"users"}, {"dimension":"last"}, {"dimension":"months"}]
-        all_in(parsed,expected)
+    # def pytest_2(self):
+    #     parsed = parser.parse_request("users last month")
+    #     expected = [{"measure":"users"}, {"dimension":"last"}, {"dimension":"months"}]
+    #     all_in(parsed,expected)
 
-    def pytest_3(self):
-        parsed = parser.parse_request("tugo")
-        expected = [{"dimension":"tugo"}]
-        all_in(parsed,expected)
+    # def pytest_3(self):
+    #     parsed = parser.parse_request("tugo")
+    #     expected = [{"dimension":"tugo"}]
+    #     all_in(parsed,expected)
 
 
 
---
+++ sys/boot.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '../src/')
+
+from api import app as application
